cb57722a5aac5387c0280735138b33330e5c227b
romanporras <j.romanporras@gmail.com>
2020-04-09 06:04:33 +0200

    Prueba Sticky Footer

---
+++ packages/webwizard-tracklights/src/partials/StickyFooter.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles((theme) => ({
+    footer: {
+      padding: theme.spacing(3, 2),
+      marginTop: 'auto',
+      backgroundColor:
+        theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
+    },
+  }));
+
+export default function StickyFooter() {
+    const classes = useStyles();
+    
+  return (
+    <footer className={classes.footer}>
+        <Container maxWidth="sm">
+            <Typography variant="body1">My sticky footer can be found here.</Typography>
+        </Container>
+    </footer>
+  );
+}
