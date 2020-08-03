8d50e9c3be4996f2828a5162c57d33d2345f9de3
jgimenoperez <jgimenoperez@gmail.com>
2020-08-03 13:02:46 +0200

    20

--- producer.js
+++ producer.js
@@ -5,8 +5,8 @@ sock.bindSync('tcp://10.1.0.101:3000');
 console.log('Producer bound to port 3000');
 
 setInterval(function(){
-  console.log('sending work 1');
-  sock.send('some work 1');
+  console.log('sending work 2');
+  sock.send('some work 2');
 }, 500);
 
 // WORKERS----->conecta con----->PRODUCERSF
