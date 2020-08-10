33dca6a4ebb462c2e68914601acdbfbb0664b537
jgimenoperez <jgimenoperez@gmail.com>
2020-08-10 09:08:37 +0200

    20

--- producer.js
+++ producer.js
@@ -1,16 +1,15 @@
-var zmq = require('./node_modules/zeromq');
-var sock = zmq.socket('push');
+var zmq = require("./node_modules/zeromq");
+var sock = zmq.socket("push");
 
-sock.bindSync('tcp://10.1.0.101:3000');
-console.log('Producer bound to port 3000');
+sock.bindSync("tcp://10.1.0.101:3000");
+console.log("Producer bound to port 3000");
 
-setInterval(function(){
-  console.log('sending work 19');
-  sock.send('some work 19');
+setInterval(function () {
+  console.log("sending work 20");
+  sock.send("some work 20");
 }, 500);
 
-
-//GIT HUB
+//GIT HUBP
 // WORKERS----->conecta con----->PRODUCERSF
 //console.log('tcp://' + process.env.ipworker +':3000');
 //sock.bindSync('tcp://10.1.0.100:3000');
